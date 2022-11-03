409ffde8a5a879a6416a0372ac5aa8446b97c4cc
ty-hal <tyhalepn16@gmail.com>
2022-11-03 17:15:36 -0400

    Edit
    Edited gui.py and data_processing.py files

---
+++ data_processing.py
@@ -0,0 +1,26 @@
+import pandas as pd # Python library for data analysis and data frame
+import numpy as np # Numerical Python library for linear algebra and computations
+pd.set_option('display.max_columns', None) # code to display all columns
+import warnings
+warnings.filterwarnings('ignore') # To prevent kernel from showing any warning
+
+df = pd.read_csv('input/movies.csv') #Read the CSV file
+df.drop_duplicates(inplace=True) #Remove duplicate entries
+df.drop_duplicates(subset=['title','release_date'], inplace=True) #Remove duplicate movies (same title AND release date)
+#659,978 movies left at this point
+
+new_df = df[df.vote_count >= 10].reset_index() #Remove movies with less than 10 vote counts (can be adjusted)
+new_df.fillna('', inplace=True) #Replace NaN values with ''
+index = new_df[(new_df['overview']=='') & (new_df['overview'] == '')].index #Find indices of movies with blank overviews (plots) and genres
+new_df.drop(index, inplace=True) #Remove these movies
+#64,560 movies left at this point (vote count filter removes a lot)
+
+#Genres, keywords, and credits delimiters are '-' so change it to ' '
+new_df['genres'] = new_df['genres'].apply(lambda x: ' '.join(x.split('-')))
+new_df['keywords'] = new_df['keywords'].apply(lambda x: ' '.join(x.split('-')))
+new_df['credits'] = new_df['credits'].apply(lambda x: ' '.join(x.replace(' ', '').split('-')[:5]))
+new_df['title_no_spaces'] = new_df['title'].str.replace('[^a-zA-Z0-9]', '') #Add a column called "title_no_spaces" that is the alphanumeric title of the movie (remove non-important characters)
+new_df['info'] = new_df['overview'] + ' ' + new_df['keywords'] #Add a column called "info" that stores the overview (plot) and keywords (used for comparing plots between movies)
+
+new_df.drop(columns=['index', 'production_companies', 'budget', 'revenue', 'status', 'backdrop_path', 'vote_count', 'recommendations'], inplace = True) #Remove columns we don't need to save space
+new_df.to_csv('input/processed_data.csv', index = False) #Save to CSV file
